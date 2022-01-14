669a21b56012e93ee00f87d8c2ea9a5ba6642e0a
yuan zhang <yuan.zhang2@yuans-MacBook-Pro.local>
2022-01-14 19:07:42 +0800

    停车等待时加收每分钟0.25元

--- src/main/java/tdd/taxi/Taxi.java
+++ src/main/java/tdd/taxi/Taxi.java
@@ -2,14 +2,16 @@ package tdd.taxi;
 
 public class Taxi {
 
-    private static final double UNIT_PRICE = 0.8;
     public static final int BASIC_DISTANCE = 8;
+    private static final double UNIT_PRICE_OF_DISTANCE = 0.8;
+    public static final double UNIT_PRICE_OF_WAITING = 0.25;
 
-    public double calculate(int distance) {
-        double distancePrice = UNIT_PRICE * distance;
+    public double calculate(int distance, int time) {
+        double distancePrice = UNIT_PRICE_OF_DISTANCE * distance;
         if (distance > 8) {
-            distancePrice += (distance - BASIC_DISTANCE) * (UNIT_PRICE / 2);
+            distancePrice += (distance - BASIC_DISTANCE) * (UNIT_PRICE_OF_DISTANCE / 2);
         }
-        return distancePrice;
+        double waitingPrice = time * UNIT_PRICE_OF_WAITING;
+        return distancePrice + waitingPrice;
     }
 }
--- src/test/java/tdd/taxi/TaxiTest.java
+++ src/test/java/tdd/taxi/TaxiTest.java
@@ -6,35 +6,44 @@ import static org.junit.Assert.assertEquals;
 
 //Done 不超过8公里时每公里0.8元
 //Done 超过8公里则每公里加收50%长途费
-//Todo 停车等待时加收每分钟0.25元
+//Done 停车等待时加收每分钟0.25元
 public class TaxiTest {
 
     private static final double DELTA = 0.00001;
 
     @Test
     public void should_be_0p8yuan_when_distance_is_1km() {
-        verifyCalculate(1, 0.8);
+        verifyCalculate(1, 0, 0.8);
     }
 
     @Test
     public void should_be_6p4yuan_when_distance_is_8km() {
-        verifyCalculate(8, 6.4);
+        verifyCalculate(8, 0, 6.4);
     }
 
     @Test
     public void should_be_6p4yuan_when_distance_is_9km() {
-        verifyCalculate(9, 7.6);
+        verifyCalculate(9, 0, 7.6);
     }
 
     @Test
     public void should_be_6p4yuan_when_distance_is_10km() {
-        verifyCalculate(10, 8.8);
+        verifyCalculate(10, 0, 8.8);
     }
 
+    @Test
+    public void should_be_1p05yuan_when_distance_is_8km_and_wait_for_2_minute() {
+        verifyCalculate(8, 2, 6.9);
+    }
 
-    private void verifyCalculate(int distance, double expected) {
+    @Test
+    public void should_be_1p05yuan_when_distance_is_9km_and_wait_for_2_minute() {
+        verifyCalculate(9, 2, 8.1);
+    }
+
+    private void verifyCalculate(int distance, int time, double expected) {
         final Taxi taxi = new Taxi();
-        double actual = taxi.calculate(distance);
+        double actual = taxi.calculate(distance, time);
         assertEquals(expected, actual, DELTA);
     }
 }
